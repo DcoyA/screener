0d6ae0557204af3ec74eead379f30597f8b4a6a1
MoneyShot <53429471+DcoyA@users.noreply.github.com>
2026-04-30 21:39:11 +0900

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import "./globals.css";
 
 export const metadata = {
   title: "우량주 스카우터",
-  description: "OpenDART와 KRX 데이터를 활용한 저평가 종목 스크리너 MVP"
+  description: "OpenDART와 KRX 데이터를 활용한 저평가 종목 스크리너"
 };
 
 export default function RootLayout({ children }) {
